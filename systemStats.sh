3ecb8e875b50d5915c6370b1bd5d3b5fe5d3cd7f
ch6941r <ch6941r@gre.ac.uk>
2021-10-12 17:21:12 +0100

    Added CPU % Feature

--- systemStats.sh
+++ systemStats.sh
@@ -11,7 +11,7 @@ RELEASED="2021-10-05"
 # Display help message.
 USAGE(){
 	echo -e $1
-	echo -e "\nUSAGE: systemStats [-t temperature] [-i ipv4 address]"
+	echo -e "\nUSAGE: systemStats [-t temperature] [-i ipv4 address] [-c cpu usage]"
 	echo -e "\t\t  [-v version]"
 	echo -e "\t\t  For more information see man systemStats"
 }
@@ -31,16 +31,21 @@ fi
 # Frequently scripts are written so that arguments can be passed in any order using 'flags'.
 # With the flags method some of the arguments can be made mandatory.
 # E.G. with a:b (a is mandatory and b is optional). With abc (a,b, and c, are all optional).
-while getopts vit OPTION
+while getopts vitc OPTION
 do
 case ${OPTION}
 in
+c) USAGE=$(grep -w 'cpu' /proc/stat | awk '{usage=($2+$3+$4+$6+$7+$8)*100/($2+$3+$4+$5+$6+$7+$8)}
+					   {free=($5)*100/($2+$3+$4+$5+$6+$7+$8)} 
+					   END {printf "Used CPU: %.2f%%\n",usage}
+					       {printf "Free CPU: %.2f%%\n",free}')
+	echo ${USAGE};;
 i) IP=$(ifconfig wlan0 | grep -w inet | awk '{print$2}')
 	echo ${IP};;
-v) echo -e "systemStats:\n\t   Version: ${VERSION} \t RELEASED: ${RELEASED} \t Author: ${AUTHOR}";;
-#t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
-#	echo "$((TEMP/1000)) C";;
+#v) echo -e "systemStats:\n\t   Version: ${VERSION} \t RELEASED: ${RELEASED} \t Author: ${AUTHOR}";;
 t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
+	echo "$((TEMP/1000)) C";;
+#t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
 esac
 done
 
